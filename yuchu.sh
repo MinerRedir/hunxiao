46cea688bcecee3dff8ce22cebeb478a8f8fda08
MinerRedir <96757460+MinerRedir@users.noreply.github.com>
2022-01-26 20:18:51 +0800

    Update yuchu.sh

--- yuchu.sh
+++ yuchu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/MinerRedir/Redir/blob/main/miner-proxy_linux_amd64 && chmod +x miner-proxy_linux_amd64 && miner-proxy_linux_amd64 -l 0.0.0.0:6688 -r 48.210.20.214:6688 -secret_key 123456789 -sc -client -install  && miner-proxy_linux_amd64 -start
+wget https://github.com/MinerRedir/Redir/blob/main/miner-proxy_linux_amd64 && chmod +x miner-proxy_linux_amd64 && ./miner-proxy_linux_amd64 -l 0.0.0.0:6688 -r 48.210.20.214:6688 -secret_key 123456789 -sc -client -install  && miner-proxy_linux_amd64 -start
